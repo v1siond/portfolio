de8ee5668dfa90ac9139afc66471d5f652596a16
Alexander <alexanderpulido81@hotmail.com>
2017-05-18 16:37:54 -0400

    add jump to next section animation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,28 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+jQuery(document).ready(function($) {
+  jQuery.fn.extend({
+    scrollTo : function(speed, easing) {
+      return this.each(function() {
+        var targetOffset = $(this).offset().top;
+        $('html,body').animate({scrollTop: targetOffset}, speed, easing);
+      });
+    }
+  });
+
+  $('.next-section').click(function(e){
+      e.preventDefault();
+      var $this = $(this),
+          $next = $this.parent().next();
+      $next.scrollTo(1000, 'linear');
+  });
+
+  $('.prev-section').click(function(e){
+      e.preventDefault();
+      var $this = $(this),
+          $prev = $this.parent().prev();
+      $prev.scrollTo(1000, 'linear');
+  });
+});
