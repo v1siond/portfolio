fc223eee94c2e3129e969de720ecf9b2cd51fa43
Alexander <alexanderpulido81@hotmail.com>
2017-05-18 12:06:46 -0400

    add responsive

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module PortfolioRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # add app/assets/fonts to the asset path
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
